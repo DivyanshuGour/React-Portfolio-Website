0e946e18d1e0c763901fc11afc2378f93f6bc31e
Divyanshu Gour <divyanshugour197@gmail.com>
2021-08-09 23:33:35 +0530

    third commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+          <div className="left">
+              <div className="imgContainer">
+                  <img src="assets/picsquare.jpeg" alt="" />
+              </div>
+          </div>
+          <div className="right">
+              <div className="wrapper">
+                  <h2>Hi There, I'm</h2>
+                  <h1>Divyanshu Gour</h1>
+                  <h3>Freelance<span></span></h3>
+              </div>
+              <a href="#portfolio">
+                  Down
+              </a>
+              </div>  
         </div>
     )
 }
